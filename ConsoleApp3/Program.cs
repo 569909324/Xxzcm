48017aca6af7034f44aaaf991ffca2e602438779
Star <Star@DESKTOP-4A5GO9T>
2019-07-20 11:15:34 +0800

    添加 .gitignore 和 .gitattributes。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Random = new Random().Next(100);
+
+            Console.WriteLine("请输入一个不超过100的自然数");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                int reduceTimes = 4 - i;
+                if (input == Random)
+                {
+                    Console.WriteLine($"恭喜你答对了，只用了{i + 1}次");
+                }
+                else if (input > Random)
+                {
+                    Console.WriteLine($"太大了，还剩{reduceTimes }次");
+
+                } 
+
+                else  
+                {
+                    Console.WriteLine($"太小了，还剩{reduceTimes }次");
+                }
+            }
+                    
+        }
+        
+
+    }
+}
